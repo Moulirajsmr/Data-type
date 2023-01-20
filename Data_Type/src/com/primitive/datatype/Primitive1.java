6f65c87e1276ece74051bf0acc98d894a992cf01
Moulirajsmr <mouli.rock07@gmail.com>
2023-01-20 22:26:58 +0530

    Primary

---
+++ Data_Type/src/com/primitive/datatype/Primitive1.java
@@ -0,0 +1,28 @@
+package com.primitive.datatype;
+
+public class Primitive1 {
+	
+	public static void main(String[] args) {
+		
+		byte g = -120;
+		System.out.println("the value of byte = "+ g);
+		short h = 4569;
+		System.out.println("the vale of short = "+ h);
+		int i = 456 ;
+		System.out.println("the value of int= "+ i);
+		long j = 81986;
+		System.out.println("the value of long= "+ j);
+		float k = 44.869396f;
+		System.out.println("the value of float= "+ k);
+		double l =5887.985149849114;
+		System.out.println("the value of double= "+ l);
+		boolean m = true;
+		System.out.println("the value of boolean= "+ m);
+		char n = 'Z';
+		System.out.println("the value of char= "+ n);
+		String o = "hi how are you 984651^*^&";
+		System.out.println("the value of string= "+ o);
+			
+	}
+
+}
---
+++ Data_Type/src/com/primitive/datatype/Primitive2.java
@@ -0,0 +1,25 @@
+package com.primitive.datatype;
+
+public class Primitive2 {
+	
+	public static void main(String[] args) {
+		
+		byte m = 90;
+		System.out.println("Age = " + m);
+		short n = 1900 ;
+		System.out.println("Birth year = " + n);
+		int o = 893567429 ;
+		System.out.println("Phone number = " + o);
+		long p = 9548985496436l ;
+		System.out.println("Aadhar number = " + p);
+		char q = 'M';
+		System.out.println("Gender = " +q);
+		boolean r = true ;
+		System.out.println("Citizen of India = " + r);
+		float s = 0.156454f;
+		System.out.println("Float value = " + s);
+		double t = 0.5498545894984968;
+		System.out.println("Double value = " + t);
+	}
+
+}
